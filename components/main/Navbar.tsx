16b4ab65aea4a8a2e55bbfe9251c08904956a1b4
DorianMAC <88128756+DorianCMA@users.noreply.github.com>
2024-06-30 01:30:10 -0400

    update

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -1,8 +1,15 @@
+'use client';
 import { Socials } from "@/constants";
 import Image from "next/image";
-import React from "react";
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
@@ -19,12 +26,12 @@ const Navbar = () => {
           />
 
           <span className="font-bold ml-[10px] hidden md:block text-gray-300">
-            Dorian Dev 
+            Dorian Dev
           </span>
         </a>
 
-        <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
-          <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
+        <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20 hidden sm:flex">
+          <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200 ">
             <a href="#about-me" className="cursor-pointer">
               About me
             </a>
@@ -35,6 +42,23 @@ const Navbar = () => {
               Projects
             </a>
           </div>
+        </div>
+
+        <div className="flex sm:hidden flex-row items-center">
+          <button onClick={toggleMenu} className="focus:outline-none">
+          {isMenuOpen ? (
+                    <Image src="/close.svg" width={30} height={30} alt="logo" />
+                  ) : (
+                    <Image
+                      src="/hamburger-menu.svg"
+                      width={30}
+                      height={30}
+                      alt="logo"
+                      className="focus:border-none active:border-none cursor-pointer text-gray-200"
+                    />
+                  )}
+            
+          </button>
         </div>
 
         <div className="flex flex-row gap-5">
@@ -49,6 +73,20 @@ const Navbar = () => {
           ))}
         </div>
       </div>
+
+      {isMenuOpen && (
+        <div className="sm:hidden w-full bg-[#0300145e] px-10 py-5">
+          <a href="#about-me" className="block py-2 text-gray-200">
+            About me
+          </a>
+          <a href="#skills" className="block py-2 text-gray-200">
+            Skills
+          </a>
+          <a href="#projects" className="block py-2 text-gray-200">
+            Projects
+          </a>
+        </div>
+      )}
     </div>
   );
 };
